18232e0111e3de19e0808473c8d67f8a7b014a2a
JoseFonck <josev.fons@gmail.com>
2023-01-21 22:17:44 -0300

    apartado siembra y modificaciones

--- siembra.js
+++ siembra.js
@@ -1,3 +1,37 @@
 
 
-console.log(location.pathname);
+const wizard = document.querySelector(".wizard");
+
+const button_cultivo = document.querySelector(".button_cultivo")
+
+
+
+//desplega los li
+document.querySelector(".a").addEventListener('click', (e)=>{
+    e.preventDefault()
+    let ul = document.querySelector(".ul");
+    ul.style.display = "block";
+    wizard.style.display = "block";
+})
+
+//oculta los li
+wizard.addEventListener('click', (e)=>{
+    e.preventDefault()
+    let ul = document.querySelector(".ul");
+    ul.style.display = "none";
+    wizard.style.display = "none";
+})
+
+button_cultivo.addEventListener('click', (e)=>{
+    e.preventDefault();
+    let ul = document.querySelector("#cultivos");
+    ul.style.display = "block";
+    wizard.style.display = "block";
+})
+
+wizard.addEventListener('click', (e)=>{
+    e.preventDefault()
+    let ul = document.querySelector("#cultivos");
+    ul.style.display = "none";
+    wizard.style.display = "none";
+})
